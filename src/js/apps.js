b154b9de473560a6fb2c05eb0e5257c46468aa83
meanmail <meanmail@mail.ru>
2017-03-04 17:37:25 +0700

    Added method getParam(name) for apps object

--- src/js/apps.js
+++ src/js/apps.js
@@ -1,23 +1,16 @@
 /**
  * Created by meanmail on 22.02.17.
  */
-(function () {
+window.apps = new function () {
     'use strict';
     var widgets = window.widgets = {};
     var title = "Stepik Apps";
 
-    var params = [];
+    var params = getParams();
 
-    var parts = window.location.search.substr(1).split("&");
-
-    for (var index in parts) {
-        var pair = parts[index].split("=");
-        params[pair[0]] = decodeURIComponent(pair[1]);
-    }
-
-    if (!!params["code"]) {
-        stepik.authorize(params["state"], params["code"]);
-    }
+    this.getParam = function (name) {
+        return params[name];
+    };
 
     function loadWidget(name) {
         $.ajax({
@@ -34,46 +27,44 @@
     loadWidget("app");
     loadWidget("appheader");
 
-    function Apps() {
-    }
-
-    $(document).ready(function () {
-        $.ajax({
-            url: 'apps/apps.json',
-            dataType: "json",
-            success: function (data) {
-                window.apps = new Apps();
+    $(document).ready((function (apps) {
+        return function () {
+            $.ajax({
+                url: 'apps/apps.json',
+                dataType: "json"
+            }).done(function (data) {
                 apps.applications = data.applications;
                 apps.categories = data.categories;
 
-                initCategories();
+                initCategories(apps);
 
-                if (!!!params["app"]) {
-                    drawApplications();
+                if (!apps.getParam("app")) {
+                    var category = apps.getParam("category");
+                    drawApplications(apps, category);
                 } else {
-                    loadApplication(params["app"]);
+                    loadApplication(apps, apps.getParam("app"));
                 }
-            }
-        });
+            });
 
-        updateUserName();
-    });
+            updateUserName();
+        }
+    })(this));
 
-    function initCategories() {
+    function initCategories(apps) {
+        var categories = $("#categories");
         apps.categories.forEach(function (item) {
-            $("#categories").append("<li><div class='category' category_id='" + item.id + "'>" + item.name + "</div></li>");
+            categories.append("<li><div class='category' category_id='" + item.id + "'>" + item.name + "</div></li>");
         });
 
         $(".category").click(function (event) {
             var id = event.currentTarget.getAttribute("category_id");
-            drawApplications(id);
-            event.stopPropagation()
+            location.href = location.origin + (id == "undefined" ? "" : "?category=" + id);
         });
     }
 
     updateUserName();
 
-    function drawApplications(category) {
+    function drawApplications(apps, category) {
         var content = $("#content");
         content.empty();
         category = parseInt(category);
@@ -92,33 +83,31 @@
             content.append(processTemplate("${widget.app}", app));
         }
 
-        $(".app").click(openApplication);
+        $(".app").click(function (event) {
+            var id = event.currentTarget.getAttribute("app_id");
+            location.search = "?app=" + id;
+        });
+
         $("title").text(title);
         updateUserName();
     }
 
-    function openApplication(event) {
-        var id = event.currentTarget.getAttribute("app_id");
-        loadApplication(id);
-        event.stopPropagation()
-    }
-
-    function loadApplication(id, redirect_app) {
+    function loadApplication(apps, id, redirect_app) {
         var content = $("#content");
         content.empty();
 
         var app = apps.applications[id];
 
-        if (!!!app) {
+        if (!app) {
             return;
         }
 
         if (app.need_authorization && $.cookie("access_token") == null) {
-            loadApplication("login", id);
+            loadApplication(apps, "login", id);
             return;
         }
 
-        if (!!!app.content) {
+        if (!app.content) {
             $.ajax({
                 url: 'apps/' + app.id + '/content.html',
                 dataType: "html",
@@ -142,20 +131,30 @@
     }
 
     function updateUserName() {
-        if ($.cookie("access_token") != null) {
-            stepik.getCurrentUser().done(function (data) {
-                var user = data.users[0];
-                var first_name = user.first_name;
-                var last_name = user.last_name;
-                $("#user-name").text((first_name + " " + last_name).trim());
-                $("#user-avatar").attr("src", user.avatar);
-            });
-        } else {
+        function setAnonymousUser() {
             $("#user-name").html("<a class='authorize' href='javascript:void(0)'>Sign in</a>");
             $("#user-avatar").attr("src", "img/default_avatar.png");
             $(".authorize").click(function () {
                 stepik.authorize(location.origin);
             });
+        }
+
+        if ($.cookie("access_token") != null) {
+            stepik.getCurrentUser().done(function (data) {
+                var user = data.users[0];
+
+                if (!user.is_guest) {
+                    var first_name = user.first_name;
+                    var last_name = user.last_name;
+                    $("#user-name").text((first_name + " " + last_name).trim());
+                    $("#user-avatar").attr("src", user.avatar);
+                } else {
+                    $.removeCookie('access_token', {path: '/'});
+                    setAnonymousUser();
+                }
+            });
+        } else {
+            setAnonymousUser();
         }
     }
 
@@ -182,4 +181,21 @@
 
         return template;
     }
-})();
+
+    function getParams() {
+        var params = {};
+
+        var parts = window.location.search.substr(1).split("&");
+
+        for (var index in parts) {
+            var pair = parts[index].split("=");
+            params[pair[0]] = decodeURIComponent(pair[1]);
+        }
+
+        if (!!params["code"]) {
+            stepik.authorize(params["state"], params["code"]);
+        }
+
+        return params;
+    }
+};
