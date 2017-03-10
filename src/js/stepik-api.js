0ea90b84cee83ac1f66acabae5b908ce5cd8753e
meanmail <meanmail@mail.ru>
2017-03-10 21:28:08 +0700

    UnPagination api

--- src/js/stepik-api.js
+++ src/js/stepik-api.js
@@ -1,94 +1,192 @@
 /**
  * Created by meanmail on 25.02.17.
  */
+(function () {
+    'use strict';
 
-window.stepik = {
-    getCurrentUser: function () {
-        return this.getJson("api/stepics/1");
-    },
+    function UnPagination(field, query, limit) {
+        var status = "pending";
+        var members = [];
+        var page_count = 0;
+        limit = limit || 10;
+        getPage(1, this);
 
-    getJson: function (url) {
-        var access_token = $.cookie("access_token");
-        var token_type = $.cookie("token_type");
-
-        return $.get({
-            url: "https://stepik.org/" + url,
-            dataType: "json",
-            headers: {
-                "Authorization": token_type + " " + access_token
+        function getPage(page) {
+            page_count++;
+            if (page_count > limit) {
+                done();
             }
-        });
-    },
 
-    postJson: function (url, data) {
-        var access_token = $.cookie("access_token");
-        var token_type = $.cookie("token_type");
+            query(page)
+                .done(function (data) {
+                    members = members.concat(data[field]);
+                    if (data.meta.has_next) {
+                        getPage(++page);
+                    } else {
+                        done();
+                    }
+                })
+                .fail(fail)
+        }
 
-        return $.post({
-            url: "https://stepik.org/" + url,
-            dataType: "json",
-            data: JSON.stringify(data),
-            headers: {
-                "Authorization": token_type + " " + access_token
-            },
-            contentType: "application/json; charset=UTF-8"
-        })
-    },
+        var onDone = [];
+        var onFail = [];
+        var onAlways = [];
 
-    getMembers: function (group) {
-        return this.getJson("api/members?group=" + group);
-    },
+        function done() {
+            onDone.forEach(function (fulfilled) {
+                fulfilled(members);
+            });
+            always();
+            status = "fulfilled";
+        }
 
-    addMembers: function (group, user) {
-        return this.postJson("api/members", {
-            member: {
-                group: group,
-                user: user
-            }
-        });
-    },
+        function fail() {
+            onFail.forEach(function (rejected) {
+                rejected(members);
+            });
+            always();
+            status = "rejected";
+        }
 
-    getCourse: function (course_id) {
-        return this.getJson("api/courses/" + course_id);
-    },
-
-    authorize: function (state, code) {
-        var redirect_uri = "http://apps.stepik.org";
-        var client_id = "gTUqSYtRjiT9wnrmCYEWKDR2ZfeDOcdlNN8Q0Avc";
-        if (!code) {
-            state = state || location.origin;
-            var encoded_state = btoa(state + "::" + Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000);
-            $.cookie("state", encoded_state);
-            window.location.href = "https://stepik.org/oauth2/authorize/" +
-                "?client_id=" + client_id +
-                "&redirect_uri=" + redirect_uri +
-                "&scope=write" +
-                "&state=" + encoded_state +
-                "&response_type=code";
-        } else {
-            return $.post({
-                url: "https://stepik.org/oauth2/token/",
-                dataType: "json",
-                data: {
-                    grant_type: "authorization_code",
-                    client_id: client_id,
-                    code: code,
-                    redirect_uri: redirect_uri
-                }
-            }).done(function (data) {
-                if (!!state && state == decodeURIComponent($.cookie("state"))) {
-                    var expires = data.expires_in;
-                    $.cookie("access_token", data.access_token, {expires: expires});
-                    $.cookie("refresh_token", data.refresh_token, {expires: expires});
-                    $.cookie("token_type", data.token_type, {expires: expires});
-                    $.cookie("scope", data.scope, {expires: expires});
-                    var decoded_state = atob(state);
-                    var separator_pos = decoded_state.indexOf("::");
-                    location.href = decoded_state.substring(0, separator_pos)
-                } else {
-                    location.href = location.origin;
-                }
+        function always() {
+            onAlways.forEach(function (always) {
+                always(members);
             });
         }
+
+        this.done = function (done) {
+            if (status == "fulfilled") {
+                done(members);
+            } else if (status == "pending") {
+                onDone.push(done);
+            }
+            return this;
+        };
+
+        this.fail = function (fail) {
+            if (status == "rejected") {
+                fail(members);
+            } else if (status == "pending") {
+                onFail.push(fail);
+            }
+
+            return this;
+        };
+
+        this.always = function (always) {
+            if (status == "fulfilled" || status == "rejected") {
+                always(members);
+            } else if (status == "pending") {
+                onAlways.push(always);
+            }
+
+            return this;
+        };
     }
-};
+
+    window.stepik = {
+        getCurrentUser: function () {
+            return this.getJson("api/stepics/1");
+        },
+
+        getJson: function (url) {
+            var access_token = $.cookie("access_token");
+            var token_type = $.cookie("token_type");
+
+            return $.get({
+                url: "https://stepik.org/" + url,
+                dataType: "json",
+                headers: {
+                    "Authorization": token_type + " " + access_token
+                }
+            });
+        },
+
+        postJson: function (url, data) {
+            var access_token = $.cookie("access_token");
+            var token_type = $.cookie("token_type");
+
+            return $.post({
+                url: "https://stepik.org/" + url,
+                dataType: "json",
+                data: JSON.stringify(data),
+                headers: {
+                    "Authorization": token_type + " " + access_token
+                },
+                contentType: "application/json; charset=UTF-8"
+            })
+        },
+
+        getCourses: function (enrolled) {
+            return new UnPagination("courses",
+                (function (context) {
+                    return function (page) {
+                        return context.getJson("api/courses?enrolled=" + !!enrolled + "&page=" + page);
+                    };
+                })(this));
+        },
+
+        getMembers: function (group) {
+            return new UnPagination("members",
+                (function (context) {
+                    return function (page) {
+                        return context.getJson("api/members?group=" + group + "&page=" + page);
+                    }
+                })(this));
+        },
+
+        addMembers: function (group, user) {
+            return this.postJson("api/members", {
+                member: {
+                    group: group,
+                    user: user
+                }
+            });
+        },
+
+        getCourse: function (course_id) {
+            return this.getJson("api/courses/" + course_id);
+        },
+
+        authorize: function (state, code) {
+            var redirect_uri = "https://apps.stepik.org";
+            var client_id = "gTUqSYtRjiT9wnrmCYEWKDR2ZfeDOcdlNN8Q0Avc";
+            if (!code) {
+                state = state || location.origin;
+                var encoded_state = btoa(state + "::" + Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000);
+                $.cookie("state", encoded_state);
+                window.location.href = "https://stepik.org/oauth2/authorize/" +
+                    "?client_id=" + client_id +
+                    "&redirect_uri=" + redirect_uri +
+                    "&scope=write" +
+                    "&state=" + encoded_state +
+                    "&response_type=code";
+            } else {
+                return $.post({
+                    url: "https://stepik.org/oauth2/token/",
+                    dataType: "json",
+                    data: {
+                        grant_type: "authorization_code",
+                        client_id: client_id,
+                        code: code,
+                        redirect_uri: redirect_uri
+                    }
+                }).done(function (data) {
+                    if (!!state && state == decodeURIComponent($.cookie("state"))) {
+                        var expires = data.expires_in;
+                        $.cookie("access_token", data.access_token, {expires: expires});
+                        $.cookie("refresh_token", data.refresh_token, {expires: expires});
+                        $.cookie("token_type", data.token_type, {expires: expires});
+                        $.cookie("scope", data.scope, {expires: expires});
+                        var decoded_state = atob(state);
+                        var separator_pos = decoded_state.indexOf("::");
+                        location.href = decoded_state.substring(0, separator_pos)
+                    } else {
+                        location.href = location.origin;
+                    }
+                });
+            }
+        }
+    };
+})();
