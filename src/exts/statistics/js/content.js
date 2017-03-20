0bc23b44ec4f71e964509cb0a3d48501fa07d394
meanmail <meanmail@mail.ru>
2017-03-20 16:57:10 +0700

    Initial statistics extension

---
+++ src/exts/statistics/js/content.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meanmail on 20.03.17.
+ */
+
+;'use strict';
+
+window.extensions.register("statistics", new function () {
+    var EXT_ID = "statistics";
+
+    this.init = function () {
+    }
+});
