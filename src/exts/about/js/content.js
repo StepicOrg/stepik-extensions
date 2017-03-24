54481978e72adb03de7696c6ffcc8d92f30f3824
meanmail <meanmail@mail.ru>
2017-03-24 17:57:09 +0700

    Create about extensions

---
+++ src/exts/about/js/content.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meanmail on 24.03.17.
+ */
+;'use strict';
+
+window.extensions.register("about", new function () {
+    var EXT_ID = "about";
+
+    this.init = function (redirect_ext) {
+
+    };
+});
