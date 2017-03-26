08dc6ab439f952e47a77ec5996b70c709dc1ae58
meanmail <meanmail@mail.ru>
2017-03-26 23:29:43 +0700

    Added stub for unknown extensions

---
+++ src/exts/unknown/js/content.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meanmail on 26.03.17.
+ */
+;'use strict';
+
+window.extensions.register("unknown", new function () {
+    var EXT_ID = "unknown";
+
+    this.init = function () {
+
+    };
+});
