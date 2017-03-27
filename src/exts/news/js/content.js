e84743e3d91605dad51c17e72f4a6b36b1ad5e48
meanmail <meanmail@mail.ru>
2017-03-27 16:10:17 +0700

    Added news extension

---
+++ src/exts/news/js/content.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meanmail on 27.03.17.
+ */
+;'use strict';
+
+window.extensions.register("news", new function () {
+    var EXT_ID = "news";
+
+    this.init = function () {
+
+    };
+});
