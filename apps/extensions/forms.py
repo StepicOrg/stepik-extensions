4ecfba134aee1096c34390d17718ad0a1577d514
meanmail <meanmail@mail.ru>
2017-05-01 20:22:01 +0700

    Packages with several extensions

--- apps/extensions/forms.py
+++ apps/extensions/forms.py
@@ -1,10 +1,12 @@
 import json
+import os
+import zipfile
 from zipfile import ZipFile, BadZipFile
 
 from django import forms
 
 from apps.extensions.models import Extension
-from apps.stepik_auth.utils import check_package_json
+from apps.extensions.utils import check_package_json
 
 
 # noinspection PyClassHasNoInit
@@ -15,37 +17,70 @@ class ExtensionUploadForm(forms.Form):
         cleaned_data = self.cleaned_data
         source = cleaned_data['source']
 
-        try:
-            with ZipFile(source) as zip_package:
-                with zip_package.open('package.json') as package:
-                    content = str(package.read(), encoding='UTF-8')
-                    try:
-                        package_json = json.loads(content)
-                    except ValueError:
-                        cleaned_data['source'] = None
-                        self.add_error('source', "package.json is not JSON")
+        if not zipfile.is_zipfile(source):
+            self.set_source_error(cleaned_data, "File is not zip")
+        else:
+            packages = dict()
+            try:
+                with ZipFile(source) as zip_package:
+                    if 'package.json' in zip_package.namelist():
+                        package, ext_id = self.check_package(zip_package, cleaned_data, 'package.json')
+                        if package is not None:
+                            packages[ext_id] = package
                     else:
-                        errors = check_package_json(package_json)
+                        for filename in zip_package.namelist():
+                            path = filename.split('/')
+                            depth = len(path)
+                            if depth == 2 and path[1] == 'package.json':
+                                package, ext_id = self.check_package(zip_package, cleaned_data, filename)
+                                if package is None:
+                                    break
+                                package['path'] = os.path.join(*path[:-1])
+                                packages[ext_id] = package
+            except BadZipFile:
+                self.set_source_error(cleaned_data, "File is not zip")
 
-                        if errors is not None and len(errors) > 0:
-                            cleaned_data['source'] = None
-                            self.add_error('source', errors)
-                        else:
-                            ext_id = package_json['id']
-                            try:
-                                Extension.objects.get(pk=ext_id)
-                                cleaned_data['source'] = None
-                                self.add_error('source', "Extension exists with same id")
-                            except Extension.DoesNotExist:
-                                cleaned_data['package.json'] = package_json
-                                with zip_package.open(package_json['logo']) as file:
-                                    cleaned_data['logo'] = file.read()
-
-        except BadZipFile:
-            cleaned_data['source'] = None
-            self.add_error('source', 'File is not zip')
-        except KeyError as e:
-            cleaned_data['source'] = None
-            self.add_error('source', str(e))
+            cleaned_data['packages'] = packages
 
         return cleaned_data
+
+    def set_source_error(self, cleaned_data, message):
+        cleaned_data['source'] = None
+        self.add_error('source', message)
+
+    def check_package(self, zip_package, cleaned_data, filename):
+        log = ["Checking " + filename]
+        try:
+            with zip_package.open(filename) as package:
+                content = str(package.read(), encoding='UTF-8')
+                try:
+                    package_json = json.loads(content)
+                except ValueError:
+                    log += [filename + " is not JSON"]
+                    self.set_source_error(cleaned_data, log)
+                    return None, None
+                else:
+                    errors = check_package_json(package_json)
+
+                    if errors is not None and len(errors) > 0:
+                        log += errors
+                        self.set_source_error(cleaned_data, log)
+                        return None, None
+                    else:
+                        ext_id = package_json['id']
+                        try:
+                            Extension.objects.get(pk=ext_id)
+                            log += ["Extension exists with same id"]
+                            self.set_source_error(cleaned_data, log)
+                            return None, None
+                        except Extension.DoesNotExist:
+                            package = dict()
+                            package['package.json'] = package_json
+                            path = filename.split('/')
+                            logo_filename = '/'.join(path[0:-1] + [package_json['logo']])
+                            with zip_package.open(logo_filename) as file:
+                                package['logo'] = file.read()
+                            return package, ext_id
+        except KeyError as e:
+            self.set_source_error(cleaned_data, str(e))
+            return None, None
